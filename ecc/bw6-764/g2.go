957a4f1554f0a75cb6bf292bff5bb127b4100d0e
Youssef El Housni <youssef.housni21@gmail.com>
2021-09-06 14:24:43 +0100

    fix(bw6-764): multiexp partitionscalar API

--- ecc/bw6-764/g2.go
+++ ecc/bw6-764/g2.go
@@ -19,6 +19,7 @@ package bw6764
 import (
 	"math"
 	"math/big"
+	"runtime"
 
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark-crypto/ecc/bw6-764/fp"
@@ -887,7 +888,7 @@ func BatchScalarMultiplicationG2(base *G2Affine, scalars []fr.Element) []G2Affin
 		baseTable[i].AddMixed(base)
 	}
 
-	pScalars := partitionScalars(scalars, c)
+	pScalars := partitionScalars(scalars, c, false, runtime.NumCPU())
 
 	// compute offset and word selector / shift to select the right bits of our windows
 	selectors := make([]selector, nbChunks)
